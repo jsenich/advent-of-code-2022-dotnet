3445d39ac43da1779f0d20f4e1cb8587544a66eb
Jay Senich <1821282+jsenich@users.noreply.github.com>
2022-12-01 00:37:12 -0500

    Day 1 complete

---
+++ Day01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace Day01;
+class Program
+{
+    static int PartOne(string[] puzzleInput)
+    {
+        int currentElf = 0;
+        List<int> elfCalories = new List<int>() { 0 };
+        foreach (var line in puzzleInput)
+        {
+            if (line == "")
+            {
+                currentElf++;
+                elfCalories.Add(0);
+                continue;
+            }
+            elfCalories[currentElf] += Int32.Parse(line);
+        }
+
+        return elfCalories.Max();
+    }
+
+    static int PartTwo(string[] puzzleInput)
+    {
+        int currentElf = 0;
+        List<int> elfCalories = new List<int>() { 0 };
+        foreach (var line in puzzleInput)
+        {
+            if (line == "")
+            {
+                currentElf++;
+                elfCalories.Add(0);
+                continue;
+            }
+            elfCalories[currentElf] += Int32.Parse(line);
+        }
+        return elfCalories.OrderDescending().Take(3).Sum();
+    }
+
+    static void Main(string[] args)
+    {
+        var input = File.ReadAllLines("input.txt");
+
+        Console.WriteLine($"PartOne: {PartOne(input)}");  // 74394
+        Console.WriteLine($"PartOne: {PartTwo(input)}"); // 212836
+    }
+}
