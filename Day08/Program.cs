d72f920040dd112ae79fc9dd7f0bdacbd4d286e0
Jay Senich <1821282+jsenich@users.noreply.github.com>
2022-12-08 10:12:44 -0500

    Day 8 part 1 complete

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -4,9 +4,17 @@ class Program
     static void Main(string[] args)
     {
         var puzzleInput = File.ReadAllLines("input.txt");
+        //         var puzzleInput = """
+        // 30373
+        // 25512
+        // 65332
+        // 33549
+        // 35390
+        // """.Trim().Split("\n");
 
         var rows = puzzleInput.Length;
         var cols = puzzleInput[0].Length;
+        HashSet<string> seenTrees = new HashSet<string>();
 
         int[,] treeGrid = new int[rows, cols];
 
@@ -22,34 +30,82 @@ class Program
 
         for (int i = 1; i < rows - 1; i++)
         {
-            var largest = Enumerable.Range(1, rows - 1).Select(x => treeGrid[i, x]).Max();
-            if (largest > treeGrid[i, 0])
+            var largest = Enumerable.Range(0, rows - 1).Select(x => treeGrid[i, x]).Max();
+            var lastTree = treeGrid[i, 0];
+
+            for (int j = 1; j < cols - 1; j++)
             {
-                visibleTrees++;
+                if (treeGrid[i, j] > lastTree)
+                {
+                    seenTrees.Add(String.Join('-', i.ToString(), j.ToString()));
+                    lastTree = treeGrid[i, j];
+                    // visibleTrees++;
+                    if (treeGrid[i, j] == largest)
+                    {
+                        break;
+                    }
+                }
             }
 
-            if (largest > treeGrid[i, cols - 1])
+            lastTree = treeGrid[i, cols - 1];
+            for (int j = cols - 2; j >= 1; j--)
             {
-                visibleTrees++;
+                if (treeGrid[i, j] > lastTree)
+                {
+                    seenTrees.Add(String.Join('-', i.ToString(), j.ToString()));
+                    lastTree = treeGrid[i, j];
+                    // visibleTrees++;
+                    if (treeGrid[i, j] == largest)
+                    {
+                        break;
+                    }
+                }
             }
         }
+
 
         for (int i = 1; i < cols - 1; i++)
         {
-            var largest = Enumerable.Range(1, cols - 1).Select(y => treeGrid[y, i]).Max();
-            if (largest > treeGrid[0, i])
+            var largest = Enumerable.Range(0, cols - 1).Select(x => treeGrid[x, i]).Max();
+            var lastTree = treeGrid[0, i];
+
+            for (int j = 1; j < rows - 1; j++)
             {
-                visibleTrees++;
+                if (treeGrid[j, i] > lastTree)
+                {
+                    seenTrees.Add(String.Join('-', j.ToString(), i.ToString()));
+                    lastTree = treeGrid[j, i];
+                    // visibleTrees++;
+                    if (treeGrid[j, i] == largest)
+                    {
+                        break;
+                    }
+                }
             }
 
-            if (largest > treeGrid[cols - 1, i])
+            lastTree = treeGrid[rows - 1, i];
+            for (int j = rows - 2; j >= 1; j--)
             {
-                visibleTrees++;
+                if (treeGrid[j, i] > lastTree)
+                {
+                    seenTrees.Add(String.Join('-', j.ToString(), i.ToString()));
+                    lastTree = treeGrid[j, i];
+                    // visibleTrees++;
+                    if (treeGrid[j, i] == largest)
+                    {
+                        break;
+                    }
+                }
             }
         }
 
 
-        // Console.WriteLine($"PartOne: {}");
-        // Console.WriteLine($"PartTwo: {}");
+        Console.WriteLine($"PartOne: {visibleTrees + seenTrees.Count}"); // 1796
+
+
+        HashSet<int> scenicScores = new HashSet<int>();
+
+
+        Console.WriteLine($"PartTwo: {scenicScores.Max()}");
     }
 }
