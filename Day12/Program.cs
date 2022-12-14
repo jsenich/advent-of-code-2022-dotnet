039d7bfc3bdd9b6a64ed47ac3ce412ec6d36326b
Jay Senich <1821282+jsenich@users.noreply.github.com>
2022-12-13 20:30:05 -0500

    Add day 12 project

---
+++ Day12/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Day12;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var puzzleInput = File.ReadAllLines(@"input.txt");
+
+        var heightmap = new char[puzzleInput.Length, puzzleInput[0].Length];
+
+        (int y, int x) start;
+        (int y, int x) end;
+
+        for (var row = 0; row < heightmap.GetLength(0); row++)
+        {
+            for (var col = 0; col < heightmap.GetLength(1); col++)
+            {
+                if (puzzleInput[row][col] == 'S')
+                {
+                    start = (row, col);
+                }
+                else if (puzzleInput[row][col] == 'E')
+                {
+                    end = (row, col);
+                }
+
+                heightmap[row, col] = puzzleInput[row][col];
+            }
+        }
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}
