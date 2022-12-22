3919d018df201cac8b542b984ace2be5345a3f66
Jay Senich <1821282+jsenich@users.noreply.github.com>
2022-12-22 10:24:43 -0500

    Even slower day 20 part 2

--- Day20/Program.cs
+++ Day20/Program.cs
@@ -32,6 +32,38 @@ public static class MyExtensions
 
     }
 
+    public static void Rotate<T>(this List<T> list, long count)
+    {
+        if (count == 0) { return; }
+        count %= list.Count;
+        if (count == 0) return;
+        long left = count < 0 ? -count : list.Count + count;
+        long right = count > 0 ? count : list.Count - count;
+
+        if (left <= right)
+        {
+            for (int i = 0; i < left; i++)
+            {
+                var temp = list[0];
+                list.RemoveAt(0);
+                list.Add(temp);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < right; i++)
+            {
+                var temp = list[list.Count - 1];
+                list.RemoveAt(list.Count - 1);
+                list.Insert(0, temp);
+            }
+        }
+
+    }
+}
+
+class Program
+{
     static int PartOne(string[] puzzleInput)
     {
         var numbers = puzzleInput.Select((n, i) => (i, int.Parse(n))).ToList<(int pos, int n)>();
@@ -75,26 +107,50 @@ public static class MyExtensions
         return groove1.n + groove2.n + groove3.n;
     }
 
-
-    class Program
+    static long PartTwo(string[] puzzleInput)
     {
-        static void Main()
+        var numbers = puzzleInput.Select((n, i) => ((long)i, long.Parse(n) * 811589153)).ToList<(long pos, long n)>();
+        var mixedNumbers = numbers.ToList();
+        var size = numbers.Count;
+
+        foreach (var _ in Enumerable.Range(0, 10))
         {
-            var puzzleInput = File.ReadAllText("input.txt").Trim().Split("\n", StringSplitOptions.RemoveEmptyEntries & StringSplitOptions.TrimEntries);
+            foreach (var num in numbers)
+            {
+                var currentIndex = mixedNumbers.IndexOf(num);
 
-            // var puzzleInput = """
-            // 1
-            // 2
-            // -3
-            // 3
-            // -2
-            // 0
-            // 4
-            // """.Split("\n", StringSplitOptions.RemoveEmptyEntries & StringSplitOptions.TrimEntries);
-
-
-
-            Console.WriteLine($"Part One: {PartOne(puzzleInput)}"); // 13289
+                mixedNumbers.Rotate(-currentIndex);
+                mixedNumbers.RemoveAt(0);
+                mixedNumbers.Rotate(-num.n);
+                mixedNumbers.Insert(0, num);
+            }
         }
+
+        var zeroStart = mixedNumbers.FindIndex(x => x.n == 0);
+
+        var groove1 = mixedNumbers[(zeroStart + 1000) % size];
+        var groove2 = mixedNumbers[(zeroStart + 2000) % size];
+        var groove3 = mixedNumbers[(zeroStart + 3000) % size];
+
+        return groove1.n + groove2.n + groove3.n;
+    }
+    static void Main()
+    {
+        var puzzleInput = File.ReadAllText("input.txt").Trim().Split("\n", StringSplitOptions.RemoveEmptyEntries & StringSplitOptions.TrimEntries);
+
+        // var puzzleInput = """
+        // 1
+        // 2
+        // -3
+        // 3
+        // -2
+        // 0
+        // 4
+        // """.Split("\n", StringSplitOptions.RemoveEmptyEntries & StringSplitOptions.TrimEntries);
+
+
+        // Console.WriteLine($"Part One: {PartOne(puzzleInput)}"); // 13289
+        Console.WriteLine($"Part One: {PartTwo(puzzleInput)}"); //
     }
 }
+
